15e54e88476b4b4d7d26a059395cb7b99dea9a7e
Ren Hao <renhao2k@gmail.com>
2019-01-16 23:22:29 +0800

    initialize repo.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE_NAME='gitlab-ce'
+
+function build() {
+  local tag=$1
+  docker build -t "${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:$tag" .
+  if [[ -n "${DOCKER_PASSWORD}" ]]; then
+      echo "Publish image '${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:$tag' to Docker Hub ..."
+      docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+      docker push "${DOCKER_USERNAME}/${DOCKER_IMAGE_NAME}:$tag"
+  fi
+}
+
+build 11.6.3-zh
